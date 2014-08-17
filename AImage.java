33d9d5ef56b5fc897467fa84cc128ba1fb9c74b7
WesleyyC <wesely.chin0919@gmail.com>
2014-08-18 07:23:31 +0800

    Initial

---
+++ AImage.java
@@ -0,0 +1,149 @@
+// This is a program that edits a picture.
+
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+
+public class AImage {
+
+	public BufferedImage im = null; //don't modify this
+	public int[] packedData = null; //don't modify this
+	public int[][][] pixelData = null; //use this to modify the image
+	public int height = 0; //read but do not set this
+	public int width = 0; //read but do not set this
+
+	//Constructor. This allows a program to initialize an instance of
+	//this class by using the 'new' keyword.
+	public AImage(BufferedImage image) {
+		im = image;
+		height = im.getHeight();
+		width = im.getWidth();
+		System.out.println(width);
+		System.out.println(height);
+		packedData = im.getRGB(0, 0, width, height, null, 0, width);
+		unpackPixels();
+	}
+
+	// Flip the picture horizontally.
+	public void flipHorizontal()
+	{
+		for (int i = 0; i < pixelData.length; i++)
+		{
+			for (int j = 0; j <= (pixelData[i].length - 1)/2; j++)	// only need to go through half-way point.
+			{
+				int k = pixelData[i].length - 1 - j;
+				int[] temp = pixelData[i][j];
+				pixelData[i][j] = pixelData[i][k];
+				pixelData[i][k] = temp;
+			}
+		}
+
+		System.out.println("Flipped Horizontally.");
+	}
+
+	// Flip the picture vertically.
+	public void flipVertical()
+	{
+		for (int i = 0; i <= (pixelData.length - 1)/2; i++)	// only need to go through half-way point.
+		{
+			for (int j = 0; j < pixelData[i].length; j++)
+			{
+				int k = pixelData.length - 1 - i;
+				int[] temp = pixelData[i][j];
+				pixelData[i][j] = pixelData[k][j];
+				pixelData[k][j] = temp;
+			}
+		}
+
+		System.out.println("Flipped Vertically.");
+	}
+
+	// Invert the color of the picture.
+	public void invert()
+	{
+		for (int i = 0; i < pixelData.length; i++)
+		{
+			for (int j = 0; j < pixelData[i].length; j++)
+			{
+				pixelData[i][j][0] = Math.abs (pixelData[i][j][0] - 255);
+				pixelData[i][j][1] = Math.abs (pixelData[i][j][1] - 255);
+				pixelData[i][j][2] = Math.abs (pixelData[i][j][2] - 255);
+			}
+		}
+
+		System.out.println("Inverted.");
+	}
+
+	// Replace the color in a certain range.
+	public void replaceColor(int[] oldColor, int[] newColor, int range)
+	{
+		for (int i = 0; i < pixelData.length; i++)
+		{
+			for (int j = 0; j < pixelData[i].length; j++)
+			{
+				// Every channel needs to be in the range.
+				if (oldColor[0] - range < pixelData[i][j][0] && pixelData[i][j][0] < oldColor[0] + range && oldColor[1] - range < pixelData[i][j][1] && pixelData[i][j][1] < oldColor[1] + range && oldColor[2] - range < pixelData[i][j][2] && pixelData[i][j][2] < oldColor[2] + range)
+				{
+					pixelData[i][j] = newColor;
+				}
+			}
+		}
+
+		System.out.println("Replaced color");
+	}
+
+
+	//Writes the current data in pixelData to a .png image by first packing
+	//the data into a 1D array of ints, then calling the write() method of
+	//the ImageIO class.
+	public boolean writeImage(File file) {
+		//put pixelData into packedData
+		packPixels();
+
+		//Write new packed array back into BufferedImage
+		//bi.setRGB(startX, startY, w, h, rgbArray, offset, scansize)
+		im.setRGB(0, 0, width, height, packedData, 0, width);
+
+		try{
+			ImageIO.write(im, "png", file);
+		} catch (IOException e) {
+			System.out.println("Writing image failed.");
+			return false;
+		}
+		return true;
+	}
+
+
+	//Uses bitwise operations to convert one integer into four channels,
+	//each with a range from 0 to 255.
+	public void unpackPixels() {
+		System.out.println("Getting pixel values from packed data...");
+
+		//This is a rows x columns array. That is, it is an array of rows.
+		pixelData = new int[height][width][3];
+
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col ++) {
+				pixelData[row][col][0] = (packedData[(row * width) + col] >> 16) & 0xff;
+				pixelData[row][col][1] = (packedData[(row * width) + col] >> 8) & 0xff;
+				pixelData[row][col][2] = (packedData[(row * width) + col]) & 0xff;
+			}
+		}
+	}
+
+	//Uses bitwise operations to convert four integer (ranging from 0 to 255)
+	//into a single integer for use with the BufferedImage class.
+	public void packPixels() {
+		System.out.println("putting pixel values in packed format...");
+
+		for (int row = 0; row < height; row ++) {
+			for (int col = 0; col < width; col ++) {
+				packedData[(row * width) + col] = ((255 & 0xFF) << 24) | //alpha
+	            ((pixelData[row][col][0] & 0xFF) << 16) | //red
+	            ((pixelData[row][col][1] & 0xFF) << 8)  | //green
+	            ((pixelData[row][col][2] & 0xFF) << 0); //blue
+			}
+		}
+	}
+}
---
+++ Editor.java
@@ -0,0 +1,181 @@
+import java.util.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class Editor {
+
+	//For use in printing messages to user.
+	public static final String[] COLOR = {"Red", "Green", "Blue"};
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to command line image editor.");
+		System.out.println("Type 'help' for usage information or, if you know how to use it, just type your command at the prompt.\n\n");
+
+		//initialize scanner for all user interaction
+		Scanner console = new Scanner(System.in);
+
+		//enter primary loop
+		interact(console);
+	}
+
+	//The main loop that reads user input and calls methods of the image
+	public static void interact(Scanner console) {
+		//The sole AImage instance this program uses.
+		AImage im1 = null;
+
+		while (true) {
+			System.out.println("Enter command:");
+			//read user input as a string and convert to lower case
+			String command = console.next().toLowerCase();
+			//read extraneous input
+			console.nextLine();
+
+			//This block of if / else-if statements controls the flow of the
+			//program based on user input. There are other ways to do this
+			//but for a project of this scale, if-statements are a decent option.
+			if (command == "help") {
+				displayHelp();
+			} else if (command.equals("load")) {
+				im1 = loadImage(console);
+				if (im1 == null) {
+					System.out.println("Image load failed.");
+				} else {
+					System.out.println("Image successfully loaded.");
+				}
+			} else if (command.equals("save")) {
+				if (im1 == null) {
+					System.out.println("No loaded image to write.");
+				} else {
+					System.out.println("Enter the file path where the image should be saved:");
+					if (im1.writeImage(new File(console.next().toLowerCase()))) {
+						System.out.println("Image successfully saved.");
+					}
+				}
+			} else if (command.equals("quit")) {
+				System.out.println("Terminating.");
+				break;
+
+			//The rest of these clauses handle the operations
+			//that can be performed on an image
+			} else if (command.equals("flip-horiz")) {
+				if (im1 != null) {
+					im1.flipHorizontal();
+				} else {
+					System.out.println("No image loaded to flip.");
+				}
+			} else if (command.equals("flip-vert")) {
+				if (im1 != null) {
+					im1.flipVertical();
+				} else {
+					System.out.println("No image loaded to flip.");
+				}
+			} else if (command.equals("invert")) {
+				if (im1 != null) {
+					im1.invert();
+				} else {
+					System.out.println("No image loaded to invert.");
+				}
+			} else if (command.equals("replace")) {
+				if (im1 != null) {
+					//Get additional user input
+					System.out.println("Enter integers for the color to replace:");
+					int[] oldColor = getColor(console);
+					if (oldColor == null) {
+						continue;
+					}
+					System.out.println("Enter integers for the new color:");
+					int[] newColor = getColor(console);
+					if (newColor == null) {
+						continue;
+					}
+					System.out.println("Enter an integer specifying how large a range of colors to replace:");
+					int range;
+					try {
+						range = console.nextInt();
+					} catch (InputMismatchException e) {
+						System.out.println("Input was not a valid input type for range");
+						continue;
+					}
+
+					//if program reaches this point, input is valid, so call replace
+					im1.replaceColor(oldColor, newColor, range);
+
+				} else {
+					System.out.println("No image loaded to replace.");
+				}
+			} else {
+				System.out.println("command '" + command + "' not understood. Type 'help' for usage information.");
+			}
+		}
+	}
+
+
+	//Reads in three integer values representing the red, green, and blue
+	//channels of a color. Also does error checking to keep values between 0 and 255.
+	public static int[] getColor(Scanner console) {
+		int[] rgb = new int[3];
+		for (int i = 0; i < 3; i ++) {
+			System.out.println(COLOR[i] + " (0 - 255):");
+			try {
+				rgb[i] = console.nextInt();
+				if (rgb[i] < 0 || rgb[i] > 255) {
+					System.out.println("Input out of range 0 - 255");
+					return null;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Inproper input type.");
+				return null;
+			}
+		}
+		return rgb;
+	}
+
+	//Prints usage information for this program.
+	public static void displayHelp() {
+		System.out.println("Usage:");
+		System.out.println("'help' -- Displays this list of commands.");
+		System.out.println("'quit' -- Causes the program to terminate.");
+		System.out.println("'load' -- Prompts for image file name and loads that image.\n" +
+								"Replaces any image currently in memory.");
+		System.out.println("'save' -- Prompts for filename and writes current image to file.");
+		System.out.println("'flip-horiz' -- Calls the flipHorizontal() method of the current image.");
+		System.out.println("'flip-vert' -- Calls the flipVertical() method of the current image.");
+		System.out.println("'invert' -- Calls the invert() method of the current image.");
+		System.out.println("'replace' -- Calls the replaceColor() method of the current image.\n" +
+							"Subsequently prompts for two colors and a range.");
+	}
+
+	//Tries to load an image file into a AImage instance.
+	//Returns null if load fails.
+	public static AImage loadImage(Scanner console) {
+		//load file
+		System.out.println("Enter the name of the file you wish to load:");
+
+		BufferedImage bi = null;
+		InputStream in = null;
+
+		//get file path from user
+		File f = new File(console.next());
+
+		//initialize stream from file
+		try {
+			in = new FileInputStream(f);
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't find file " + f.getAbsolutePath());
+			return null;
+		}
+
+		//read stream into BufferedImage
+		try {
+			bi = ImageIO.read(in);
+		} catch (IOException e) {
+			System.out.println("Failed to read FileInputStream.");
+			return null;
+		}
+
+		return new AImage(bi);
+
+	}
+
+}
